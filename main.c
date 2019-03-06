1947d0c2107091a32465fecc04255cbdb62a3d11
Theagh <ugur.agakisiyev@gmail.com>
2019-03-06 10:49:32 +0400

    Basic getopt structs added

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <time.h>
+
+
+#define DEF_INTERVAL 10000
+
+int main(int argc, char const *argv[])
+{
+    char c;
+    unsigned int interval=DEF_INTERVAL, limit=0;
+    bool ret_code=false;
+	while((c=getopt(argc, argv, "+t:i:l:c"))!=-1){
+		
+		switch (c)
+		{
+			case 'i':
+                interval=atoi(optarg);
+                break;
+			case 'l':
+                limit=atoi(optarg);
+				break;
+            case 'c':
+                ret_code=true;
+				break;
+			case '?':
+				// help();
+				break;
+            case 'h':
+                // help();
+                exit(0);
+			default:
+				
+				exit(-1);
+				break;
+		}
+	}
+    return 0;
+}
